c1f10f81da6292bf7e26b68f0e5e091fa136f672
jainisheridan <133915511+jainisheridan@users.noreply.github.com>
2023-08-10 16:36:33 -0400

    Update WarRunnerGame.java

--- src/main/java/com/mycompany/warrunnergame/WarRunnerGame.java
+++ src/main/java/com/mycompany/warrunnergame/WarRunnerGame.java
@@ -14,13 +14,55 @@ import java.util.Scanner;
 public class WarRunnerGame {
 
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter password with minimum lenght 7, Password with atleast one uppercase, one special character");
+        
         System.out.print("Enter Player 1 name: ");
         String playerName1 = scanner.nextLine();
+        
+        System.out.print("Enter Password: ");
+        String password = scanner.next();
+        
+        if(checkLength(password)&& checkUpper(password) && checkSpecial(password))
+        {
+            System.out.println("Valid Password");
+        }
+        else{
+            System.out.println("InValid Password");
+        }
+        
         int maxRounds = 26; // Set the desired number of rounds here
         WarGame warGame = new WarGame(playerName1);
         warGame.playGame(maxRounds);
     }
+        
+        public static boolean checkLength(String password){
 
+            if(password.length() > 7)
+            {
+              return true;  
+            }
+            return false;
+        }
+        public static boolean checkUpper(String password) {
+            for (char letter : password.toCharArray()) {
+                if (Character.isUpperCase(letter)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static boolean checkSpecial(String password) {
+            for (char letter : password.toCharArray()) {
+                if (!Character.isLetterOrDigit(letter)) {
+                    return true;
+                }
+            }
+            return false;
+        }
 }
+
 
